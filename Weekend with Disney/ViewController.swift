5c4e5e43d81b2b30542a2fe4d92a302bfc0ff149
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2017-11-25 04:05:12 -0600

    11/25/17

--- Weekend with Disney/ViewController.swift
+++ Weekend with Disney/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewOneBackButton: UIButton!
+    @IBOutlet weak var viewOneNextButton: UIButton!
+    @IBOutlet weak var viewOneSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func viewOneBackButton(_ sender: Any) {
+    }
+    @IBAction func viewOneNextButton(_ sender: Any) {
+    }
+    
 
 }
 
